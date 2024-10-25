137da3b23da699f176a95f71bde6e4173e319da5
Ravid Efroni <ravidefroni@gmail.com>
2024-10-25 15:20:55 +0200

    Add placeholders for nav and footer

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
-import Image from 'next/image';
+import Footer from './components/footer';
+import Header from './components/header';
 
 export default function Home() {
-  return <div>Hellow World</div>;
+  return (
+    <>
+      <div>
+        <Header />
+        Hellow World
+      </div>
+      <Footer />
+    </>
+  );
 }
