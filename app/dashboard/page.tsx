f15bd3a38ee9408c023ffd9ed746faeca9ad2460
Ravid Efroni <ravidefroni@gmail.com>
2024-11-19 17:24:19 +0100

    Improve dashboard logic to allow only one salary entry

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -98,23 +98,27 @@ export default async function DashboardPage() {
             })}
 
             {/* Card 2 - Add New Entry */}
-            <Link href="/dashboard/survey" className="card card-button">
-              <div className="icon-container-empty">
-                <svg
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
-                  <line x1="12" y1="8" x2="12" y2="16" />
-                  <line x1="8" y1="12" x2="16" y2="12" />
-                </svg>
-              </div>
-              <p className="cta-add-salary">Enter Salary</p>
-            </Link>
+            {jobDetails.length > 0 ? (
+              ''
+            ) : (
+              <Link href="/dashboard/survey" className="card card-button">
+                <div className="icon-container-empty">
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+                    <line x1="12" y1="8" x2="12" y2="16" />
+                    <line x1="8" y1="12" x2="16" y2="12" />
+                  </svg>
+                </div>
+                <p className="cta-add-salary">Enter Salary</p>
+              </Link>
+            )}
           </div>
         </div>
       </div>
