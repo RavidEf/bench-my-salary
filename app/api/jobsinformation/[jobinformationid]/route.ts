a6959a39001cf2465a3ed2184b3e92bbfbae0e7e
Ravid Efroni <ravidefroni@gmail.com>
2024-11-26 08:54:21 +0100

    Change PUT type paramas

--- app/api/jobsinformation/[jobinformationid]/route.ts
+++ app/api/jobsinformation/[jobinformationid]/route.ts
@@ -7,7 +7,7 @@ import {
 } from '../../../../migrations/00010-createtablejobinformation';
 
 type JobsParams = {
-  params: { jobInformationId: string };
+  params: Promise<{ jobInformationId: string }>;
 };
 
 export type MainSurveyResponseBodyPut =
